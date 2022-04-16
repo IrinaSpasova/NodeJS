9ed50a206323774041a1c5f1e54bb234d2b2a45f
Irina Spasova <irina.spasova.81@gmail.com>
2022-04-16 22:13:47 +0300

    Authorize Permissions Setup

--- JohnSmilga/Tutorials/10-e-commerce-api/starter/routes/userRoutes.js
+++ JohnSmilga/Tutorials/10-e-commerce-api/starter/routes/userRoutes.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const {authenticateUser} = require('../middleware/authentication');
-const { getAllUsers,getSingleUser,showCurrentUser,updateUser,updateUserPassword } = require('../controllers/userController');
+const {
+    authenticateUser,
+    authorizePermissions
+} = require('../middleware/authentication');
+const {
+    getAllUsers,
+    getSingleUser,
+    showCurrentUser,
+    updateUser,
+    updateUserPassword
+} = require('../controllers/userController');
 
 
-router.route('/').get(authenticateUser,getAllUsers); 
+router.route('/').get(authenticateUser, authorizePermissions, getAllUsers); // IMPORTANT - we have to put authorizePermissions after authenticateUser, because we have to identify user firs to be able to check his role
 
 router.route('/showMe').get(showCurrentUser);
 
@@ -12,7 +21,7 @@ router.route('/updateUser').patch(updateUser);
 
 router.route('/updateUserPassword').patch(updateUserPassword);
 
-router.route('/:id').get(authenticateUser,getSingleUser);
+router.route('/:id').get(authenticateUser, getSingleUser);
 
 
 
