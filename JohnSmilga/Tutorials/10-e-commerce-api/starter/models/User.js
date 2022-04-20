a4c13e0d3f3999bc53f04e3320687e94e40a93bd
Irina Spasova <irina.spasova.81@gmail.com>
2022-04-20 22:19:47 +0300

    updateUser with user.save()

--- JohnSmilga/Tutorials/10-e-commerce-api/starter/models/User.js
+++ JohnSmilga/Tutorials/10-e-commerce-api/starter/models/User.js
@@ -32,6 +32,11 @@ const UserSchema = new mongoose.Schema({
 });
 
 UserSchema.pre('save',async function(){
+    // console.log('PROVERKA');  test for updateUser with user.save()
+    //console.log(this.modifiedPaths());
+    //console.log(this.isModified('name'));
+
+    if(!this.isModified('password')) return;
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password,salt);
 });
